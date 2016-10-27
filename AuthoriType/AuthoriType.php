b8447e01674363f80e015f8f1465255f54126ca9
CarterBland <carter_bland2000@yahoo.com>
2016-10-27 11:56:20 -0500

    Update AuthoriType.php

--- AuthoriType/AuthoriType.php
+++ AuthoriType/AuthoriType.php
@@ -1,25 +1,25 @@
 <?php
 /*AuthoriType Created By Carter Bland @ carterbland.com
-
 */
+
 class atype
-{	
-	//constraints are read as 0 = true 1 = false; change
+{
+    //constraints are read as 0 = true 1 = false; change
     public $constraints = array('headers' => '0', 'textcolor' => '0', 'links' => '0', 'images' => '0', 'ordered lists' => '0', 'codesnippets' => '0');
     
     //Creates a custom class called by $newclass -> new atype(array())
     public function __construct($const)
     {
-    	//Checks if there are any constraints
+        //Checks if there are any constraints
         if (isset($const[0])) {
-        	//Cycles through constraints to compare
+            //Cycles through constraints to compare
             foreach ($this->constraints as $superkey => $superval) {
-            	//Cycles through the parameter $const
+                //Cycles through the parameter $const
                 foreach ($const as $setkey => $setval) {
-                	//Sets $constraints[$superconst] if your parameter contains an array that matches it
-                	if ($setval == $superkey) {
-                		$this->constraints[$superkey] = "1";
-                	}
+                    //Sets $constraints[$superconst] if your parameter contains an array that matches it
+                    if ($setval == $superkey) {
+                        $this->constraints[$superkey] = "1";
+                    }
                 }
             }
         } // If no const it's assumed all are allowed
