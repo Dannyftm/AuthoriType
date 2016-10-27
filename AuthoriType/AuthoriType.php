0ccfbc85e15166796b0806302c7f5d55843a26ea
CarterBland <carter_bland2000@yahoo.com>
2016-10-27 17:49:21 -0500

    Fixed Header Parsing
    next tweaking links and creating textcolor parsing

--- AuthoriType/AuthoriType.php
+++ AuthoriType/AuthoriType.php
@@ -1,46 +1,83 @@
 <?php
 /*
-    AuthoriType Created By Carter Bland @ carterbland.com
+	AuthoriType Created By Carter Bland @ carterbland.com
+	
+	To Use:
+	require('AuthoriType.php');
+	
+	$textenv = new atype(array());
+	echo $textenv->parse(text); (Outputs HTML)
+	
+	XSS:
+	Be default everything is parsed, to escape things add keys to the array with the desired escape key
+	example: $textenv = new atype(array('images','links')); - escapes links and images
 */
-
 class atype
-{
-    //constraints are read as 0 = true 1 = false; change
-    public $constraints = array('headers' => '0', 'textcolor' => '0', 'links' => '0', 'images' => '0', 'ordered lists' => '0', 'codesnippets' => '0');
+{	
+	//constraints are read as 0 = true 1 = false; change
+    public $constraints = array(
+    	'headers' => '0',
+    	'textcolor' => '0',
+    	'links' => '0',
+    	'images' => '0',
+    	'ordered lists' => '0',
+    	'codesnippets' => '0'
+    );
     
     //Creates a custom class called by $newclass -> new atype(array())
     public function __construct($const)
     {
-        //Checks if there are any constraints
+    	//Checks if there are any constraints
         if (isset($const[0])) {
-            //Cycles through constraints to compare
+        	//Cycles through constraints to compare
             foreach ($this->constraints as $superkey => $superval) {
-                //Cycles through the parameter $const
+            	//Cycles through the parameter $const
                 foreach ($const as $setkey => $setval) {
-                    //Sets $constraints[$superconst] if your parameter contains an array that matches it
-                    if ($setval == $superkey) {
-                        $this->constraints[$superkey] = "1";
-                    }
+                	//Sets $constraints[$superconst] if your parameter contains an array that matches it
+                	if ($setval == $superkey) {
+                		$this->constraints[$superkey] = '1';
+                	}
                 }
             }
-        } // If no const it's assumed all are allowed
-    }
+        } 
+    } // If no const it's assumed all are allowed
     
+    //Attaches Function To The Class
     public function parse($text)
     {
-        if ($this->constraints['headers'] == "0") {
-            $text = preg_replace('/(?!*)[*]{1}(\w[a-zA-Z0-9 ]+)[*]{1}(?<!*)/', "<h3 style='margin:0px;top:5px'>" . '$1' . "</h3>", $text);
-            $text = preg_replace('/(?!*)[*]{2}(\w[a-zA-Z0-9 ]+)[*]{2}(?<!*)/', "<h4 style='margin:0px;top:5px'>" . '$1' . "</h4>", $text);
+    	//Escapes Important Characters
+    	$text = preg_replace('/[<]/','&lt;',$text);
+    	$text = preg_replace('/[>]/','&gt;',$text);
+    	$text = preg_replace('/["]/','&quot;',$text);
+    	$text = preg_replace("/[']/",'&#39;',$text);
+    	$text = preg_replace('/[&]/','&amp;',$text);
+    	
+    	//Parses Headers
+        if ($this->constraints['headers'] == '0') {
+			$text = preg_replace('/[#]{2}[ ]{1}(\w[a-zA-Z0-9 ]+)/', '<h1 style="margin:0px;top:5px">' . '$2' . '</h1>', $text);
+			$text = preg_replace('/[#]{2}([2-6]{1})[ ]{1}(\w[a-zA-Z0-9 ]+)/', '<h$1 style="margin:0px;top:5px">$2</h$1>', $text);
         }
+        
+        //Parses Links
+        if ($this->constraints['links'] == '0') {
+			$text = preg_replace('/[[]{1}(\w[a-zA-Z0-9 \-\.\_\~\:\/\?\\#\[\]\@\!\$\&\(\)\*\+\,\;\=\']+)[]][(](\w+)[)]/', '<a href="$1">$2</a>', $text);
+        }
+        
+        //Parses TextColor
+        if ($this->constraints['textcolor'] == '0') {
+            //RGB
+            $text = preg_replace('/test/','<span style="color:rgb($1,$2,$3)">$4</span>',$text);
+            //Hex Colors
+            $text = preg_replace('/test/','<span style="color:$1">$2</span>',$text);
+        }
+        
+        //Returns Parsed HTML
         return $text;
     }
 }
 
-/*
-Example
-
-Creates a a new atype
-$test = new atype(array('images','textcolor'));
-Sends through the new params
-echo $test->parse('*hi*');
-*/
+$text = new atype(array(''));
+echo $text->parse('
+##2 This is a test
+[http://google.com](clie)
+');
